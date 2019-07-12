77a8245b38c7c01628ce6816cfc73ccb84b95fcf
meghanakonuganti <meghanaraj1016@gmail.com>
2019-07-12 09:46:14 +0530

    Third Task

---
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -0,0 +1,20 @@
+package com.stackroute.domain;
+
+public class Movie {
+    private Actor actor;
+    public Movie(){
+
+    }
+    public Movie(Actor actor) {
+        this.actor = actor;
+    }
+
+    public void setActor(Actor actor) {
+        this.actor = actor;
+    }
+
+    public void showActorInfo()
+    {
+        actor.display();
+    }
+}
