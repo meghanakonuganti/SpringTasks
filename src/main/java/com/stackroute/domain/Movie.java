f17210f83a7915fc81bf8305904c3b3d290139be
meghanakonuganti <meghanaraj1016@gmail.com>
2019-07-12 09:30:07 +0530

    second task

---
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -0,0 +1,18 @@
+package com.stackroute.domain;
+
+import java.util.List;
+
+public class Movie {
+    private List<Actor> actor;
+
+    public Movie(List<Actor> actor) {
+        this.actor = actor;
+    }
+    public void showActorInfo() {
+        for (Actor a : actor) {
+            a.display();
+        }
+
+    }
+
+}
